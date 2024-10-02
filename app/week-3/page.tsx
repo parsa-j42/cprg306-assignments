53cd040eca2bf1d0ca836638037269d12633e990
parsa_j42 <parsajfri42@gmail.com>
2024-10-01 22:56:19 -0600

    Add week-3 assignment

--- app/week-3/page.tsx
+++ app/week-3/page.tsx
@@ -3,9 +3,11 @@ import ItemList from './item-list';
 
 export default function Page() {
     return (
-        <main className="container mx-auto px-4 py-8">
-            <h1 className="mb-6 text-center">Shopping List</h1>
-            <ItemList />
-        </main>
+        <>
+            <main className="container mx-auto px-4 py-8">
+                <h1 className="mb-6 text-center">Shopping List</h1>
+                <ItemList />
+            </main>
+        </>
     );
 }
