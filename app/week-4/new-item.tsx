d43df88d3490b21176b05c31040055268ab7ba59
parsa_j42 <parsajfri42@gmail.com>
2024-10-15 21:35:30 -0600

    styling change to align with week 5

--- app/week-4/new-item.tsx
+++ app/week-4/new-item.tsx
@@ -20,20 +20,20 @@ export default function NewList() {
     return (
         <div className="flex h-screen items-center justify-center pb-60">
             <div className="p-8 rounded-xl bg-white-lg shadow-xl">
-                <p className="text-xl mb-4">Quantity: {quantity}</p>
+                <p className="text-xl mb-4 text-black">Quantity: {quantity}</p>
                 <div className="flex items-center justify-center space-x-4">
                     <button
                         onClick={decrement}
                         disabled={quantity === 1}
-                        className="w-10 h-10 flex items-center justify-center shadow-md bg-blue-100 text-blue-900 hover:bg-blue-200 rounded-md disabled:bg-gray-100 disabled:text-gray-100"
+                        className="w-10 h-10 flex items-center justify-center shadow-md bg-blue-100 text-blue-900 hover:bg-blue-200 rounded-lg disabled:bg-gray-100 disabled:text-gray-100"
                     >
                         -
                     </button>
-                    <span className="text-2xl font-bold">{quantity}</span>
+                    <span className="text-2xl font-bold text-black">{quantity}</span>
                     <button
                         onClick={increment}
                         disabled={quantity === 20}
-                        className="w-10 h-10 flex items-center justify-center shadow-md bg-blue-100 text-blue-900 hover:bg-blue-200 rounded-md disabled:bg-gray-100 disabled:text-gray-100"
+                        className="w-10 h-10 flex items-center justify-center shadow-md bg-blue-100 text-blue-900 hover:bg-blue-200 rounded-lg disabled:bg-gray-100 disabled:text-gray-100"
                     >
                         +
                     </button>
--- app/week-4/page.tsx
+++ app/week-4/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import NewItem from './new-item';
 export default function Page() {
     return (
-        <>
+        <main className="flex justify-center items-center min-h-screen bg-gray-100">
             <NewItem/>
-        </>
+        </main>
     );
 }
